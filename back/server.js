4ad76b0fdd483adca29ef72fc4d4d48845543713
jjin93 <tmdgus3901@gmail.com>
2022-02-21 14:12:36 +0900

    발화자표시

--- back/server.js
+++ back/server.js
@@ -12,20 +12,28 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-const rooms = {};
+const users = {};
+
+const socketToRoom = {};
 
 io.on('connection', (socket) => {
+  console.log(`${socket.id} 가 서버에 연결됨`);
   socket.on('join room', (roomID) => {
-    if (rooms[roomID]) {
-      rooms[roomID].push(socket.id);
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit('room full');
+        return;
+      }
+      users[roomID].push(socket.id);
     } else {
-      rooms[roomID] = [socket.id];
+      users[roomID] = [socket.id];
     }
+    socketToRoom[socket.id] = roomID;
+     // 본인을 제외한 같은 room의 user array
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
 
-    const otherUsersInRoom = rooms[roomID].filter((id) => id !== socket.id);
-    if (otherUsersInRoom) {
-      socket.emit('other user', otherUsersInRoom);
-    }
+    socket.emit('all users', usersInThisRoom);
   });
 
   socket.on('sending signal', (payload) => {
@@ -40,42 +48,58 @@ io.on('connection', (socket) => {
       id: socket.id,
     });
   });
-  socket.on('code compile', (payload) => {
-    const url = 'https://api.jdoodle.com/v1/execute';
-
-    const sendData = {
-      // put your own client id and client secret of jdoodle
-      clientId: '29c0d13db4b041ca805305faac6c9155',
-      clientSecret:
-        '73a9ee5ae33448935b7736ef5c9bdcbdf8365da265cdbc036842db09edad5372',
-      script: payload.codes,
-      stdin: '',
-      language: 'nodejs',
-      versionIndex: '3',
-    };
-
-    let response = {};
-    try {
-      response = axios({
-        method: 'post', //you can set what request you want to be
-        url,
-        data: sendData,
-      }).then((response) => {
-        rooms[payload.roomID].forEach((userID) => {
-          io.to(userID).emit('code response', response.data.output);
-        });
-      });
-    } catch (e) {
-      response = e;
-      return {
-        data: {
-          e: 'Error:404\nOops Something went wrong\n😢😞🙁',
-        },
-      };
+  // user가 연결이 끊겼을 때 처리
+  socket.on('disconnect', () => {
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+    // disconnect한 user가 포함된 roomID
+    const roomID = socketToRoom[socket.id];
+    // room에 포함된 유저
+    let room = users[roomID];
+    // room이 존재한다면(user들이 포함된)
+    if (room) {
+      // disconnect user를 제외
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
     }
+   
   });
+  // socket.on('code compile', (payload) => {
+  //   const url = 'https://api.jdoodle.com/v1/execute';
+
+  //   const sendData = {
+  //     // put your own client id and client secret of jdoodle
+  //     clientId: '29c0d13db4b041ca805305faac6c9155',
+  //     clientSecret:
+  //       '73a9ee5ae33448935b7736ef5c9bdcbdf8365da265cdbc036842db09edad5372',
+  //     script: payload.codes,
+  //     stdin: '',
+  //     language: 'nodejs',
+  //     versionIndex: '3',
+  //   };
+
+  //   let response = {};
+  //   try {
+  //     response = axios({
+  //       method: 'post', //you can set what request you want to be
+  //       url,
+  //       data: sendData,
+  //     }).then((response) => {
+  //       rooms[payload.roomID].forEach((userID) => {
+  //         io.to(userID).emit('code response', response.data.output);
+  //       });
+  //     });
+  //   } catch (e) {
+  //     response = e;
+  //     return {
+  //       data: {
+  //         e: 'Error:404\nOops Something went wrong\n😢😞🙁',
+  //       },
+  //     };
+  //   }
+  // });
 });
 
 server.listen(process.env.PORT || 8000, () =>
   console.log('server is running on port 8000')
 );
+
--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,110 +1,114 @@
 import React, { useRef, useEffect, useState, useContext } from 'react';
 import io from 'socket.io-client';
 import Peer from 'simple-peer';
+import styled from 'styled-components';
+import hark from 'hark';
 
 import Canvas from '../Components/Canvas/Canvas';
 import './Room.css';
 import { useParams } from 'react-router-dom';
 import CodeEditor from '../Components/CodeEditor/Editor';
 import { codeContext } from '../Context/ContextProvider';
-import styled from 'styled-components';
 
-var canvas;
-var context;
-
-const StyledVideo = styled.video`
-  height: 0;
-  width: 0;
+const StyledAudio = styled.audio`
+  float: left;
 `;
 
-// talker 시작
-const Video = (props) => {
+const Audio = (props) => {
   const ref = useRef();
+
+  const [color, setColor] = useState("black");
 
   useEffect(() => {
     props.peer.on('stream', (stream) => {
       ref.current.srcObject = stream;
-    });
-  }, []);
+      let options = {}
+      let speechEvents = hark(stream, options);
 
-  return <StyledVideo playsInline autoPlay ref={ref} />;
+      speechEvents.on('speaking', function () {
+        setColor("yellow"); 
+      });
+
+      speechEvents.on('stopped_speaking', function () {
+        setColor("black"); 
+      });
+    });
+
+    return () => {
+      // console.log('컴포넌트가 화면에서 사라짐.');
+    };
+  }, []);
+  return (
+    <div>
+      <StyledAudio autoPlay ref={ref} />
+      <button  style={{backgroundColor : color, float : 'left'}}>상대방 버튼</button>
+    </div>
+  );
 };
 
 const Room = (props) => {
   const [peers, setPeers] = useState([]);
-  const userVideo = useRef();
-  const partnerVideo = useRef([]);
-  const peerRef = useRef([]);
   const socketRef = useRef();
-  const userStream = useRef();
+  const userVideo = useRef();
+  const peersRef = useRef([]);
   const { roomID } = useParams();
+
   const { codes, compileResult, getCompileResult, getRoomInfo } =
     useContext(codeContext);
-
-  const [muted, setMute] = useState('Mute');
 
   function sendCode() {
     socketRef.current.emit('code compile', { codes, roomID });
   }
 
-  function handleMuteClick() {
-    userStream.current
-      .getAudioTracks()
-      .forEach((track) => (track.enabled = !track.enabled));
-
-    if (muted === 'Mute') {
-      setMute('UnMute');
-    } else {
-      setMute('Mute');
-    }
-  }
-
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true, video: false })
-      .then((stream) => {
-        // userVideo.current.srcObject = stream;
-        userStream.current = stream;
+    socketRef.current = io.connect('/');
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      userVideo.current.srcObject = stream;
 
-        socketRef.current = io.connect('/');
-        getRoomInfo(roomID);
+      let options = {};
+      let speechEvents = hark(stream, options);
 
-        socketRef.current.emit('join room', roomID);
-
-        socketRef.current.on('other user', (userIDs) => {
-          if (userIDs) {
-            const peers_ = [];
-            userIDs.forEach((userID) => {
-              const peer_ = createPeer(userID, socketRef.current.id, stream);
-
-              peerRef.current.push({
-                peerID: userID,
-                peer_,
-              });
-              peers_.push(peer_);
-            });
-            setPeers(peers_);
-          }
-        });
-
-        socketRef.current.on('user joined', (payload) => {
-          const peer_ = addPeer(payload.signal, payload.callerID, stream);
-          peerRef.current.push({
-            peerID: payload.callerID,
-            peer_,
-          });
-          setPeers((users) => [...users, peer_]);
-        });
-
-        socketRef.current.on('code response', (code) => {
-          handleCompileResult(code);
-        });
-
-        socketRef.current.on('receiving returned signal', (payload) => {
-          const item = peerRef.current.find((p) => p.peerID === payload.id);
-          item.peer_.signal(payload.signal);
-        });
+      speechEvents.on('speaking', function () {
+        console.log('말하는중');
       });
+
+      speechEvents.on('stopped_speaking', () => {
+        console.log('안말하는 중');
+      });
+      getRoomInfo(roomID);
+      socketRef.current.emit('join room', roomID);
+      socketRef.current.on('all users', (users) => {
+        const peers = [];
+        users.forEach((userID) => {
+          const peer = createPeer(userID, socketRef.current.id, stream);
+          peersRef.current.push({
+            peerID: userID,
+            peer,
+          });
+          peers.push(peer);
+        });
+        setPeers(peers);
+        console.log(peers);
+      });
+
+      socketRef.current.on('user joined', (payload) => {
+        const peer = addPeer(payload.signal, payload.callerID, stream);
+        peersRef.current.push({
+          peerID: payload.callerID,
+          peer,
+        });
+        setPeers((users) => [...users, peer]);
+      });
+
+      socketRef.current.on('code response', (code) => {
+        handleCompileResult(code);
+      });
+
+      socketRef.current.on('receiving returned signal', (payload) => {
+        const item = peersRef.current.find((p) => p.peerID === payload.id);
+        item.peer.signal(payload.signal);
+      });
+    });
   }, []);
 
   /* Below are Simple Peer Library Function */
@@ -151,25 +155,18 @@ const Room = (props) => {
   return (
     <div>
       <div>
+        <StyledAudio className="user-sound" ref={userVideo} autoPlay />
+        {peers.map((peer, index) => {
+          return <Audio key={index} peer={peer} />;
+        })}
+      </div>
+      <div>
         <button className="run-button" onClick={sendCode}>
           Run
         </button>
-
-        {/* <video autoPlay ref={userVideo} />
-                <video autoPlay ref={partnerVideo} />
-                <button onClick = {handleMuteClick}>{muted}</button> */}
-
         <Canvas />
         <CodeEditor roomID={roomID} />
-
-        <StyledVideo muted ref={userVideo} autoPlay playsInline />
-        {peers.map((peer, index) => {
-          return <Video key={index} peer={peer} />;
-        })}
-
-        {/* <button onClick={BroadCastDraw}>broadcast</button> */}
       </div>
-
       <div>
         <textarea
           className="code-result"
