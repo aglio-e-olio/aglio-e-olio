d6077ac1cb5d8f8659523a6f9c1ce53f036f7d79
Hunue-Park <70733039+Hunue-Park@users.noreply.github.com>
2022-02-25 16:17:57 +0900

    setTagdData 성공

--- client/src/Components/TagSort/TagSort.js
+++ client/src/Components/TagSort/TagSort.js
@@ -1,14 +1,38 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { codeContext } from '../../Context/ContextProvider';
 import InfoList from '../InfoList/InfoList';
+import axios from 'axios';
 
 const mockData = ['Dijkstra', 'BFS', 'DFS', '완전탐색', '분할정복', '그래프'];
 
 const TagSort = () => {
   const { nickName, getTag } = useContext(codeContext);
+  const [tagData, setTagData] = useState([]);
+  const [query, setQuery] = useState('');
 
+  /* props으로 받은 tag 처리 */
   useEffect(() => {
-    // api 요청 하는 부분. 최초 렌더링 시에 하도록.
+    axios({
+      method: 'GET',
+      url: 'http://localhost:4000/tags',
+      params: { nickname: nickName },
+    })
+      .then((res) => {
+        console.log(res.data)
+        let firstSortedData = [...res.data];
+        firstSortedData.sort((a, b) => {
+          if (a.algorithm < b.algorithm) return -1;
+          if (a.algorithm > b.algorithm) return 1;
+          return 0;
+        });
+        firstSortedData.map(data => {
+            setTagData(tagData => [...tagData, data.algorithm]);
+        });
+        // setTagData(firstSortedData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   function getInfobyTag(tags) {
@@ -17,11 +41,14 @@ const TagSort = () => {
 
   return (
     <ul class="menu bg-base-100 w-56 p-2 rounded-box">
-      {mockData.map((tags) => (
-        <li key={tags.toString()} onClick={() => getInfobyTag(tags)}>
+      {tagData.map((tags) => {
+          console.log(tags, "tags!")
+          return(
+        <li onClick={() => getInfobyTag(tags)}>
           <a>#{tags}</a>
         </li>
-      ))}
+      )})}
+      {console.log(tagData, "inthe map")}
     </ul>
   );
 };
