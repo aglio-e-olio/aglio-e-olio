18a63cab250c0baaedba2ea2e69281cb3eb84a2c
Hunue-Park <70733039+Hunue-Park@users.noreply.github.com>
2022-02-26 20:45:18 +0900

    각 icon component 분리

--- client/src/Components/Canvas/Canvas.js
+++ client/src/Components/Canvas/Canvas.js
@@ -9,6 +9,9 @@ import { useKeyboardEvents } from '../../hooks/useKeyboardEvents';
 import './Canvas.css';
 import { useState } from 'react';
 import { useEraser } from '../../hooks/useEraser';
+import PenIcon from '../Atoms/PenIcon';
+import EraserIcon from '../Atoms/EraserIcon';
+import TrashIcon from '../Atoms/TrashIcon';
 
 const date = new Date();
 
@@ -147,54 +150,17 @@ export default function Canvas({
         <ul class="menu bg-base-100 p-2 rounded-box fixed z-50 top-1/3 ">
           <li onClick={changeToPencil}>
             <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                />
-              </svg>
+              <PenIcon />
             </a>
           </li>
           <li onClick={changeToEraser}>
             <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <EraserIcon />
             </a>
           </li>
           <li onClick={clearAllLines}>
             <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
+              <TrashIcon />
             </a>
           </li>
         </ul>
