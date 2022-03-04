bb6e725a9fe40b4f3b8c21b7e42fa8c018ad7569
Hunue-Park <70733039+Hunue-Park@users.noreply.github.com>
2022-03-04 17:59:55 +0900

    create background change

--- client/src/Components/Atoms/LobbyCreate.js
+++ client/src/Components/Atoms/LobbyCreate.js
@@ -2,22 +2,23 @@ import React from 'react';
 
 const LobbyCreate = ({ clickAction, buttonName }) => {
   return (
-    <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col">
-        <img
-          src="img/studying_collaborative.jpg"
-          class="max-w-md rounded-lg shadow-2xl"
-        />
-        <div class="flex-row">
-          <h1 class="text-5xl font-bold">실시간 스터디</h1>
-          <p class="py-6 whitespace-pre-line">
-            친구들을 초대해서 함께 코드 에디터와 화이트보드를 이용해서 코딩
-            스터디를 진행해보세요! <br/>코드 실행결과도 이곳에서 바로
-            확인가능하답니다.
+    <div
+      class="hero min-h-screen"
+      style={{
+        background: 'url(/img/studying_collaborative.jpg)',
+        backgroundSize: 'cover',
+      }}
+      >
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">실시간 스터디룸</h1>
+          <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
           </p>
-          <button class="btn btn-outline btn-info" onClick={clickAction}>
-            {buttonName}
-          </button>
+          <button class="btn btn-info" onClick={clickAction}>{buttonName}</button>
         </div>
       </div>
     </div>
--- client/src/Components/Atoms/LobbyHistory.js
+++ client/src/Components/Atoms/LobbyHistory.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import url from 'socket.io-client/lib/url';
 
 const LobbyHistory = ({ clickAction, buttonName }) => {
   return (
-    <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col">
-        <img
-          src="img/history_library.jpg"
-          class="max-w-md rounded-lg shadow-2xl"
-        />
-        <div class="flex-row">
-          <h1 class="text-5xl font-bold">나의 공부기록</h1>
-          <p class="py-6 whitespace-pre-line">
-            함께 공부하며 저장했던 스냅샷과 녹화 기록을 나만의 코딩 테스트
-            라이브러리에서 확인해보세요! <br/>이전 내용에서 수정하고 싶은 부분이
-            있다면 언제든지 가능합니다.
+    <div
+      class="hero min-h-screen"
+      style={{
+          background: 'url(/img/history_library.jpg)',
+          backgroundSize: 'cover',
+        }}
+      >
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">나의 공부기록</h1>
+          <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
           </p>
-          <button class="btn btn-outline btn-info" onClick={clickAction}>
-            {buttonName}
-          </button>
+          <button class="btn btn-info" onClick={clickAction}>{buttonName}</button>
         </div>
       </div>
     </div>
