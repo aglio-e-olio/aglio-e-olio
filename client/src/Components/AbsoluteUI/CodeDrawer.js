019a3e2f94d298bc8c1255fcdf1cc17c2859e0dc
Hunue-Park <70733039+Hunue-Park@users.noreply.github.com>
2022-03-05 03:18:25 +0900

    code drawer 끝

--- client/src/Components/AbsoluteUI/CodeDrawer.js
+++ client/src/Components/AbsoluteUI/CodeDrawer.js
@@ -31,24 +31,35 @@ const CodeDrawer = ({ isOpen, setIsOpen, doc, provider }) => {
         }
       >
         <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
-          <header className="p-4 font-bold text-lg">
-            Header
-            <button
-              class="btn btn-secondary mx-3"
-              onClick={() => setIsOpen(false)}
-            >
-              close
-            </button>
-          </header>
           <button
-            class="btn absolute bottom-20 right-4 z-30"
-            onClick={sendCode}
+            class="btn btn-secondary btn-xs w-8 top-1/3 left-0 fixed z-30"
+            onClick={() => setIsOpen(false)}
           >
+            <svg
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              width="100%"
+              height="100%"
+              viewBox="0 0 24 24"
+              aria-labelledby="chevronRightIconTitle"
+              stroke="#ffffff"
+              stroke-width="5"
+              stroke-linecap="square"
+              stroke-linejoin="miter"
+              fill="none"
+              color="#ffffff"
+            >
+              {' '}
+              <title id="chevronRightIconTitle">Chevron Right</title>{' '}
+              <polyline points="10 6 16 12 10 18 10 18" />{' '}
+            </svg>
+          </button>
+          <button class="btn fixed bottom-20 right-4 z-30" onClick={sendCode}>
             Run
           </button>
           <CodeEditor doc={doc} provider={provider} />
           <textarea
-            className="w-full border-solid border-2 absolute bottom-2"
+            className="w-full border-solid border-2 fixed bottom-2"
             value={compileResult}
             placeholder={
               '코드 결과 출력 창입니다. \n현재 Javascript만 지원중입니다.'
--- client/src/Components/AbsoluteUI/RightSideNav.js
+++ client/src/Components/AbsoluteUI/RightSideNav.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const RightSideNav = ({setIsOpen}) => {
+const RightSideNav = ({ setIsOpen }) => {
   return (
-    <ul class="menu bg-neutral rounded-box absolute top-1/3 right-0 w-16">
+    <ul class="menu bg-neutral hover:bg-neutral-focus cursor-pointer rounded-box absolute top-1/3 right-0 w-8">
       <li>
-      <button class="btn btn-secondary mx-3" onClick={() => setIsOpen(true)}>Open</button>
+        <svg
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32px"
+          height="32px"
+          viewBox="0 0 24 24"
+          aria-labelledby="chevronLeftIconTitle"
+          stroke="#ffffff"
+          stroke-width="1.5"
+          stroke-linecap="square"
+          stroke-linejoin="miter"
+          fill="none"
+          color="#ffffff"
+          onClick={() => setIsOpen(true)}
+        >
+          {' '}
+          <title id="chevronLeftIconTitle">Chevron Left</title>{' '}
+          <polyline points="14 18 8 12 14 6 14 6" />{' '}
+        </svg>
       </li>
     </ul>
   );
