e7fc4ade0c649eadb771d94afcb2c5bd412c6ac8
jjin93 <tmdgus3901@gmail.com>
2022-03-08 01:56:19 +0900

    ReactToolTip 위치

--- client/src/App.js
+++ client/src/App.js
@@ -30,8 +30,8 @@ function App() {
             element={<SelfStudyRoom />}
           />
         </Routes>
+      <ReactToolTip/>
       </BrowserRouter>
-      <ReactToolTip />
     </div>
   );
 }
