d54e0da9097c66256d8d99f29f88cf04b36b7d9c
jjin93 <tmdgus3901@gmail.com>
2022-03-10 01:35:27 +0900

    left툴바 ui개선

--- client/src/Components/AbsoluteUI/LeftSideToolbar.js
+++ client/src/Components/AbsoluteUI/LeftSideToolbar.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PenIcon from '../Atoms/PenIcon';
 import EraserIcon from '../Atoms/EraserIcon';
 import TrashIcon from '../Atoms/TrashIcon';
 import RedoIcon from '../Atoms/RedoIcon';
 import UndoIcon from '../Atoms/UndoIcon';
+import './LeftSideToolbar.css'
 
 const LeftSideToolbar = ({ setIsEraser, yLines, undoManager }) => {
+  const penRef = useRef();
+  const eraserRef = useRef();
 
   const clearAllLines = React.useCallback(() => {
     yLines.delete(0, yLines.length);
@@ -21,35 +24,36 @@ const LeftSideToolbar = ({ setIsEraser, yLines, undoManager }) => {
     undoManager.redo();
   }, []);
 
-  function handleTrueEraser(e) {
-    console.log('e는?', e);
-    setIsEraser((prev) => (prev = true));
-  }
-
   function handleFalseEraser() {
+    penRef.current.className = 'btn btn-active';
+    eraserRef.current.className = 'btn';
     setIsEraser((prev) => (prev = false));
   }
 
+  function handleTrueEraser(e) {
+    eraserRef.current.className = 'btn btn-active';
+    penRef.current.className = 'btn';
+    setIsEraser((prev) => (prev = true));
+  }
 
   return (
     <div class="btn-group bg-neutral p-2 rounded-box fixed left-1 z-50 top-1/3 flex flex-col">
-      <button class="btn" onClick={handleFalseEraser}>
+      <button class="btn btn-active" onClick={handleFalseEraser} ref={penRef}>
         {<PenIcon />}
       </button>
-      <button class="btn" onClick={handleTrueEraser}>
+      <button class="btn" onClick={handleTrueEraser} ref={eraserRef}>
         <EraserIcon />
       </button>
       <button class="btn" onClick={clearAllLines}>
         {<TrashIcon />}
       </button>
-      <button class="btn" onClick={undoLine}>
+      <button class="btn " onClick={undoLine}>
         {<UndoIcon />}
       </button>
       <button class="btn" onClick={redoLine}>
         {<RedoIcon />}
       </button>
     </div>
-
   );
 };
 
