4ce4faa541dbe2ac563acafe61b9beab682da978
jjin93 <tmdgus3901@gmail.com>
2022-03-11 03:37:33 +0900

    원래대로 변경

--- client/src/Components/CodeEditor/Editor.js
+++ client/src/Components/CodeEditor/Editor.js
@@ -47,13 +47,6 @@ const CodeEditor = ({ doc, provider }) => {
         javascript(),
         keymap.of([indentWithTab]),
         yCollab(ytext, provider.awareness),
-        EditorView.updateListener.of((editorUpdate) => {
-          if (editorUpdate.docChanged) {
-            const doc = editorUpdate.state.doc;
-            const value = doc.toString();
-            extractCode(value);
-          }
-        }),
         
       ],
     });
