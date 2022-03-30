c18278ab5aec0cfb288a709715989a608a00bf3a
JUNYOUNG CHOI <cjy13753@gmail.com>
2022-03-30 05:16:21 +0000

    클라이언트에서 배포 가능

---
+++ client/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
+
+server.listen(process.env.PORT || 8080, () =>
+  console.log('server is running on port 8080')
+);
