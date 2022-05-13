f4ef4bbfcc8c6a3762e8a88313dfa3fd7602a3a6
Dokyung Kim <dokyungkim@Dokyungui-MacBookAir.local>
2022-05-13 17:43:59 +0900

    virtualized table 적용

--- client/src/Components/InfoTable/InfoTable.js
+++ client/src/Components/InfoTable/InfoTable.js
@@ -3,14 +3,13 @@ import { codeContext } from '../../Context/ContextProvider';
 import PictureIcon from '../Atoms/PictureIcon';
 import CameraIcon from '../Atoms/CameraIcon';
 import {
-  List,
   Table,
   AutoSizer,
   CellMeasurer,
   CellMeasurerCache,
   Column,
 } from 'react-virtualized';
-import 'react-virtualized/styles.css'; 
+import 'react-virtualized/styles.css';
 
 /* props로 아무것도 안 줬을 때의 컴포넌트도 따로 만들어야 할 듯. */
 function InfoTable() {
@@ -18,14 +17,20 @@ function InfoTable() {
   const cacheRef = useRef(
     new CellMeasurerCache({
       fixedWidth: true,
-      defaultHeight: 100,
+      defaultHeight: 70,
     })
   ); // 변화해도 리렌더링이 일어나지 않는 값
 
   /* Preview에서 사용할 _id 만들어주기 */
-  function handleTableClick(value) {
-    selectPreview(value._id);
+  function handleTableClick({rowData}) {
+    console.log(rowData)
+    selectPreview(rowData._id);
   }
+
+  function handleTableMouseOver() {
+    
+  }
+
 
   function rowRenderer({ key, index, style, parent }) {
     const value = searchedData[index];
@@ -38,11 +43,7 @@ function InfoTable() {
         columnIndex={0}
         rowIndex={index}
       >
-        <tr
-          class="hover"
-          onClick={() => handleTableClick(value)}
-          key={key}
-        >
+        <tr class="hover" onClick={() => handleTableClick(value)} key={key}>
           <th></th>
           <td>{value.type === 'image' ? <PictureIcon /> : <CameraIcon />}</td>
           <td>{value.title}</td>
@@ -52,31 +53,38 @@ function InfoTable() {
     );
   }
 
-  return (
-      <div
-        class="overflow-y-auto overflow-x-hidden m-auto"
-      >
-        
-          <div style={{ width: '100%', height: '74vh' }}>
-            <AutoSizer >
-              {({ width, height, registerChild }) => (
-                <Table
-                  width={width}
-                  height={height}
-                  headerHeight={60}
-                  rowHeight={cacheRef.current.rowHeight}
-                  rowCount={searchedData.length}
-                  rowGetter={({index}) => searchedData[index]}
+  const TypeCell = ({ cellData }) => (
+    <div>{cellData === 'image' ? <PictureIcon /> : <CameraIcon />}</div>
+  );
 
-                >
-                  <Column label='Type' dataKey='type' width={200} />
-                  <Column label='Title' dataKey='title' width={300} />
-                  <Column label='Save Time' dataKey='save_time' width={300} />
-                </Table>
-              )}
-            </AutoSizer>
-          </div>
-       </div>
+  return (
+    <div class="overflow-y-auto overflow-x-hidden m-auto">
+      <div style={{ width: '100%', height: '74vh' }}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <Table
+              width={width}
+              height={height}
+              headerHeight={60}
+              rowHeight={cacheRef.current.rowHeight}
+              rowCount={searchedData.length}
+              rowGetter={({ index }) => searchedData[index]}
+              onRowMouseOver={handleTableMouseOver}
+              onRowClick={handleTableClick}
+            >
+              <Column
+                label="Type"
+                dataKey="type"
+                width={200}
+                cellRenderer={TypeCell}
+              />
+              <Column label="Title" dataKey="title" width={300} />
+              <Column label="Save Time" dataKey="save_time" width={500} />
+            </Table>
+          )}
+        </AutoSizer>
+      </div>
+    </div>
   );
 }
 
